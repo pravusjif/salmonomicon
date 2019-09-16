26e21cec92e5bb08ababd085c68270808669143f
Pravus <pravusjif@gmail.com>
2019-09-15 21:12:29 -0300

    Added right hand for testing

--- src/UI.ts
+++ src/UI.ts
@@ -2,18 +2,35 @@
 const canvas = new UICanvas()
 
 export const atlas = new Texture("textures/blood-27051.png")
-let currentLeftHandY: number = -20
-const leftHandImage = new UIImage(canvas, atlas)
+let currentHandsY: number = -20
+const handsPanel = new UIContainerRect(canvas)
+handsPanel.width = '100%'
+handsPanel.height = '100%'
+handsPanel.positionY = currentHandsY
+
+const leftHandImage = new UIImage(handsPanel, atlas)
 leftHandImage.hAlign = 'left'
 leftHandImage.vAlign = 'bottom'
 leftHandImage.positionX = '15%'
-leftHandImage.positionY = currentLeftHandY
+leftHandImage.positionY = currentHandsY
 leftHandImage.sourceWidth = 95
 leftHandImage.sourceHeight = 125
 leftHandImage.sourceLeft = 462
 leftHandImage.sourceTop = 566
 leftHandImage.width = leftHandImage.sourceWidth * 4
 leftHandImage.height = leftHandImage.sourceHeight * 4
+
+const rightHandImage = new UIImage(handsPanel, atlas)
+rightHandImage.hAlign = 'right'
+rightHandImage.vAlign = 'bottom'
+rightHandImage.positionX = '-15%'
+rightHandImage.positionY = currentHandsY
+rightHandImage.sourceWidth = 102
+rightHandImage.sourceHeight = 130
+rightHandImage.sourceLeft = 791
+rightHandImage.sourceTop = 559
+rightHandImage.width = rightHandImage.sourceWidth * 4
+rightHandImage.height = rightHandImage.sourceHeight * 4
 
 class HandsMovingSystem implements ISystem {
     oscillationSpeed: number
@@ -32,7 +49,7 @@ class HandsMovingSystem implements ISystem {
         this.speedMultiplier = 1
         this.idleSpeed = 15
         this.currentSpeed = 0
-        this.oscillationPivot = currentLeftHandY
+        this.oscillationPivot = currentHandsY
         this.playerLastPos = Camera.instance.position.clone()
     }
 
@@ -48,16 +65,18 @@ class HandsMovingSystem implements ISystem {
 
             if(this.currentSpeed == 0) return
 
-            currentLeftHandY += this.currentSpeed * dt * this.speedMultiplier * (Vector3.Distance(this.playerLastPos, Camera.instance.position))
+            currentHandsY += this.currentSpeed * dt * this.speedMultiplier * (Vector3.Distance(this.playerLastPos, Camera.instance.position))
         // }
 
-        if(Math.abs(currentLeftHandY - this.oscillationPivot) >= this.oscillationLength / 2) {
-            currentLeftHandY = this.oscillationPivot + (this.oscillationLength / 2) * this.speedMultiplier
+        if(Math.abs(currentHandsY - this.oscillationPivot) >= this.oscillationLength / 2) {
+            currentHandsY = this.oscillationPivot + (this.oscillationLength / 2) * this.speedMultiplier
 
             this.speedMultiplier *= -1
         }
 
-        leftHandImage.positionY = currentLeftHandY
+        // leftHandImage.positionY = currentHandsY
+        // rightHandImage.positionY = currentHandsY
+        handsPanel.positionY = currentHandsY
         this.playerLastPos = Camera.instance.position.clone()
     }
 }
