13e746270ce1ccc5640d0e82594bad99934f7a97
nearnshaw <nearnshaw@decentraland.org>
2019-09-24 00:08:49 -0300

    ambient sound

--- src/game.ts
+++ src/game.ts
@@ -3,6 +3,7 @@ import { CreatureComponent, CreatureSystem } from './creature'
 import {createBuilderScene} from './builderScene'
 import { scatterPages } from '././book'
 import { addCandles } from './candles'
+import { AmbientSound } from './ambient'
 
 //// creature
 
@@ -19,3 +20,41 @@ createBuilderScene()
 // spawnMica()
 
 //addCandles()
+
+
+let crickets1 = new AmbientSound(
+	{position: new Vector3(8, 1, 8)},
+	"sounds/crickets1.wav",
+	0,
+	true
+)
+
+// let crickets2 = new AmbientSound(
+// 	{position: new Vector3(18, 1, 18)},
+// 	"sounds/crickets2.aiff",
+// 	0,
+// 	true
+// )
+
+// let crickets3 = new AmbientSound(
+// 	{position: new Vector3(8, 1, 18)},
+// 	"sounds/crickets3.aiff",
+// 	0,
+// 	true
+// )
+
+
+
+let owl1 = new AmbientSound(
+	{position: new Vector3(15, 4, 8)},
+	"sounds/owl1.mp3",
+	30000
+)
+
+
+// let owl2 = new AmbientSound(
+// 	{position: new Vector3(24, 4, 24)},
+// 	"sounds/owl2.wav",
+// 	7000,
+// 	true
+// )
