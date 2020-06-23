4a454d3e58c396bf366361b59913b268e0e81ce0
nearnshaw <nearnshaw@decentraland.org>
2020-06-23 15:10:22 -0300

    fix bug w clicking

--- src/book.ts
+++ src/book.ts
@@ -1,180 +1,181 @@
-import { creature, CreatureState } from "./creature";
-import { pageCounterUI, dieScreen, beingWatchedText } from "./UI";
-import { addCandles, candles } from "./candles";
-import { resetMicasHead, grabMicasHead, releaseMicasHead, micaDialogueSystem, radarMica, prepareDancingMika } from "./mica";
-import { radarMicaDialogueUIText } from "./micaUI";
-import { animatedUISystem } from "./UISpritesAnimation";
-import { domeShape } from "./builderScene";
-
+import { creature, CreatureState } from './creature'
+import { pageCounterUI, dieScreen, beingWatchedText } from './UI'
+import { addCandles, candles } from './candles'
+import {
+  resetMicasHead,
+  grabMicasHead,
+  releaseMicasHead,
+  micaDialogueSystem,
+  radarMica,
+  prepareDancingMika,
+} from './mica'
+import { radarMicaDialogueUIText } from './micaUI'
+import { animatedUISystem } from './UISpritesAnimation'
+import { domeShape } from './builderScene'
 
 @Component('page')
-export class PageComponent {
-}
+export class PageComponent {}
 
 export let pages = engine.getComponentGroup(PageComponent)
 
-export let pageCounter:number = 0
+export let pageCounter: number = 0
 export let hasAllPages: boolean = false
 
 let triggerOffset = new Vector3(0, 2, 0)
 
-export class pagePos { pos: Vector3 ; rot: Vector3 }
+export class pagePos {
+  pos: Vector3
+  rot: Vector3
+}
 
 export let pagePositions: pagePos[] = [
-	{pos: new Vector3(32.6, 1.12, 46.7), rot: new Vector3(0, 0, 0) },
-	{pos: new Vector3(34.24, 0.65, 55.64), rot: new Vector3(0, 0, 0) },
-	{pos: new Vector3(22.13, 1.12, 52.5), rot: new Vector3(0,0, 0) },
-	{pos: new Vector3(32, 0.07, 32), rot: new Vector3(0, 0, 0) },
-	 {pos: new Vector3(15.16, 0.3, 35), rot: new Vector3(0, 0, 0) },
+  { pos: new Vector3(32.6, 1.12, 46.7), rot: new Vector3(0, 0, 0) },
+  { pos: new Vector3(34.24, 0.65, 55.64), rot: new Vector3(0, 0, 0) },
+  { pos: new Vector3(22.13, 1.12, 52.5), rot: new Vector3(0, 0, 0) },
+  { pos: new Vector3(32, 0.07, 32), rot: new Vector3(0, 0, 0) },
+  { pos: new Vector3(15.16, 0.3, 35), rot: new Vector3(0, 0, 0) },
 
-	{pos: new Vector3(51, 3 , 49.15), rot: new Vector3(90, 45.22, 0) },
-	{pos: new Vector3(41.59, 4.59, 53.45), rot: new Vector3( 72, 129.8, 1.2)},
-	{pos: new Vector3(49.44, 2.15, 31.62), rot: new Vector3(90, -84.13, 0) },
-	{pos: new Vector3(53.48, 1.1, 22.27), rot: new Vector3(0, 0, 8.58) },
-	{pos: new Vector3(55.21, 0.42, 8.84), rot: new Vector3(0, 0, 0) },
-	{pos: new Vector3(17.28, 2.91, 16.42), rot: new Vector3(0,0, -90)},
-	{pos: new Vector3(25.34, 2.91 ,39.49), rot: new Vector3(0,-90, -90) },
-	{pos: new Vector3(32.72, 0.2, 23.49), rot: new Vector3(0,0, 0)},
-	{pos: new Vector3(17.87, 0.85 ,50.54), rot: new Vector3(0,0, 0) }
+  { pos: new Vector3(51, 3, 49.15), rot: new Vector3(90, 45.22, 0) },
+  //{pos: new Vector3(41.59, 4.59, 53.45), rot: new Vector3( 72, 129.8, 1.2)},
+  { pos: new Vector3(49.44, 2.15, 31.62), rot: new Vector3(90, -84.13, 0) },
+  { pos: new Vector3(53.48, 1.1, 22.27), rot: new Vector3(0, 0, 8.58) },
+  { pos: new Vector3(55.21, 0.42, 8.84), rot: new Vector3(0, 0, 0) },
+  { pos: new Vector3(17.28, 2.91, 16.42), rot: new Vector3(0, 0, -90) },
+  { pos: new Vector3(25.34, 2.91, 39.49), rot: new Vector3(0, -90, -90) },
+  { pos: new Vector3(32.72, 0.2, 23.49), rot: new Vector3(0, 0, 0) },
+  { pos: new Vector3(17.87, 0.85, 50.54), rot: new Vector3(0, 0, 0) },
 ]
 
-
 export class Page extends Entity {
-	transform: Transform
-	isPicked: boolean = false
+  transform: Transform
+  isPicked: boolean = false
 
-	constructor(
-	  transform: TranformConstructorArgs,
-	  model: GLTFShape,
-	  totalPages: number
+  constructor(
+    transform: TranformConstructorArgs,
+    model: GLTFShape,
+    totalPages: number
+  ) {
+    super()
+    this.addComponent(model)
+    this.transform = new Transform(transform)
+    this.addComponent(this.transform)
+    engine.addEntity(this)
 
-	) {
-	  super();
-	  this.addComponent(model);
-	  this.transform = new Transform(transform)
-	  this.addComponent(this.transform);
-	  engine.addEntity(this);
+    this.addComponent(new PageComponent())
+    this.addComponent(
+      new OnPointerDown((e) => {
+        if (e.hit.length > 3) return
+        this.grab(totalPages)
+      })
+    )
+  }
 
-	  this.addComponent(new PageComponent()) 
-	  this.addComponent(new OnPointerDown(e => {
-			if (e.hit.length > 3) return
-			this.grab(totalPages)
-	  	}
+  public grab(totalPages: number): void {
+    if (this.isPicked) return
 
-	  ))
-	}
+    pageCounter += 1
+    pageCounterUI.value = 'Pages: ' + pageCounter.toString() + '/5'
 
-	public grab(totalPages: number): void {
-		if (this.isPicked) return
+    this.getComponent(GLTFShape).visible = false
 
-		pageCounter += 1
-		pageCounterUI.value = "Pages: " + pageCounter.toString() + "/5"
-		
-		this.getComponent(GLTFShape).visible = false
-		
-		this.isPicked = true
-	
-		if (pageCounter >= totalPages){
-			hasAllPages = true
-			book.activateGlow()
+    this.isPicked = true
 
-			radarMicaDialogueUIText.value = "Now place me by the book and I shall cast the spell!"
-			micaDialogueSystem.enabled = false;
-		}
-	}
+    if (pageCounter >= totalPages) {
+      hasAllPages = true
+      book.activateGlow()
+
+      radarMicaDialogueUIText.value =
+        'Now place me by the book and I shall cast the spell!'
+      micaDialogueSystem.enabled = false
+    }
+  }
 }
 
 // add pages in random places
 export let neededPages = []
-export function scatterPages(totalPages: number){
-	pageCounter = 0
-	pageCounterUI.value = "Pages: " + pageCounter.toString() + "/5"
-	if (pages.entities.length > 1 ){
-		for (let page of pages.entities){
-			page.getComponent(GLTFShape).visible = true
-		}
-	}else {
-		let usedPositions: number[] = []
-		for (let i = 0; i < totalPages; i ++){
-	
-			let index = Math.floor(Math.random() * pagePositions.length)
-	
-			while (usedPositions.indexOf(index) > -1 ){
-				index = Math.floor(Math.random() * pagePositions.length)
-			}
-			usedPositions.push(index)
-			let newPage = new Page(
-				{
-					position: pagePositions[index].pos,
-					rotation: pagePositions[index].rot.toQuaternion()
-				},
-				new GLTFShape('models/PapyrusOpen_01/PapyrusOpen_01.glb'), 
-				totalPages
-			)
-			neededPages.push(newPage)
-		}
-	}	
+export function scatterPages(totalPages: number) {
+  pageCounter = 0
+  pageCounterUI.value = 'Pages: ' + pageCounter.toString() + '/5'
+  if (pages.entities.length > 1) {
+    for (let page of pages.entities) {
+      page.getComponent(GLTFShape).visible = true
+    }
+  } else {
+    let usedPositions: number[] = []
+    for (let i = 0; i < totalPages; i++) {
+      let index = Math.floor(Math.random() * pagePositions.length)
+
+      while (usedPositions.indexOf(index) > -1) {
+        index = Math.floor(Math.random() * pagePositions.length)
+      }
+      usedPositions.push(index)
+      let newPage = new Page(
+        {
+          position: pagePositions[index].pos,
+          rotation: pagePositions[index].rot.toQuaternion(),
+        },
+        new GLTFShape('models/PapyrusOpen_01/PapyrusOpen_01.glb'),
+        totalPages
+      )
+      neededPages.push(newPage)
+    }
+  }
 }
 
-export function startGame(){
-	if(creature.currentState != CreatureState.Dormant) return
+export function startGame() {
+  if (creature.currentState != CreatureState.Dormant) return
 
-	domeShape.withCollisions = true
+  domeShape.withCollisions = true
 
-	micaDialogueSystem.enabled = true;
+  micaDialogueSystem.enabled = true
 
-	book.invokeCreature()
+  book.invokeCreature()
 
-	grabMicasHead()
+  grabMicasHead()
 }
 
 // RESET GAME
-export function resetGame(){
-	if (creature.currentState == CreatureState.Hunting) {
-	
-		log("YOU LOOSE")
-		dieScreen("hunted")
+export function resetGame() {
+  if (creature.currentState == CreatureState.Hunting) {
+    log('YOU LOOSE')
+    dieScreen('hunted')
+  } else if (creature.currentState == CreatureState.Trapped) {
+    log('YOU LOOSE')
+    dieScreen('laser')
+  }
+  creature.getReset()
 
-	} else if (creature.currentState == CreatureState.Trapped){
+  beingWatchedText.value = ''
+  beingWatchedText.visible = false
+  domeShape.withCollisions = false
 
-		log("YOU LOOSE")
-		dieScreen("laser")
-	}
-	creature.getReset()
-	
-	beingWatchedText.value = ""
-	beingWatchedText.visible = false
-	domeShape.withCollisions = false
-	
-	for (let i = 0 ; i < neededPages.length; i ++){	
-		let page = neededPages[i] as Page
-		page.isPicked = false
-		page.getComponent(GLTFShape).visible = false
+  for (let i = 0; i < neededPages.length; i++) {
+    let page = neededPages[i] as Page
+    page.isPicked = false
+    page.getComponent(GLTFShape).visible = false
+  }
+  //pageCounter = pages.entities.length
+  hasAllPages = false
+  pageCounter = 0
+  pageCounterUI.visible = false
+  pageCounterUI.value = 'Pages: ' + pageCounter.toString() + '/5'
+  book.activateGlow()
 
-	}
-	//pageCounter = pages.entities.length
-	hasAllPages = false
-	pageCounter = 0
-	pageCounterUI.visible = false
-	pageCounterUI.value = "Pages: " + pageCounter.toString() + "/5"
-	book.activateGlow()
+  //candlesOnCounter = 0
+  for (let candle of candles) {
+    candle.turnOff()
+    engine.removeEntity(candle)
+  }
 
-	//candlesOnCounter = 0
-	for (let candle of candles) {
-		candle.turnOff()
-		engine.removeEntity(candle)
-	}
+  book.removeGlow()
 
-	book.removeGlow()
+  // RESET MIKA STATE
+  resetMicasHead()
 
-	// RESET MIKA STATE 
-	resetMicasHead()
-
-	micaDialogueSystem.enabled = true;
-	radarMica.enabled = false
-	animatedUISystem.enabled = false
+  micaDialogueSystem.enabled = true
+  radarMica.enabled = false
+  animatedUISystem.enabled = false
 }
-
 
 // book's glow
 const rayMaterial = new Material()
@@ -185,87 +186,87 @@ rayMaterial.roughness = 0.5
 rayMaterial.transparencyMode = 2
 rayMaterial.albedoColor = new Color4(2, 2, 3, 0.3)
 
-
 const pedestal = new Entity()
 const pedestalGLTFShape = new GLTFShape('models/Pedestal_01/Pedestal_01.glb')
 pedestal.addComponentOrReplace(pedestalGLTFShape)
 const transform_10 = new Transform({
   position: new Vector3(31.5, -0.1, 14.5),
-  scale: new Vector3(0.4, 0.4, 0.4)
+  scale: new Vector3(0.4, 0.4, 0.4),
 })
 pedestal.addComponentOrReplace(transform_10)
 engine.addEntity(pedestal)
-
 
 // book of salmonomicon
 export const totalPages = 5
 
 export class Book extends Entity {
-	glow: BoxShape
-	constructor(
-	  transform: TranformConstructorArgs,
-	  model: GLTFShape
-	) {
-	  super()
-	  this.addComponent(model)
-	  this.addComponent(new Transform(transform))
-	  engine.addEntity(this)
-	  this.setParent(pedestal)
+  glow: BoxShape
+  constructor(transform: TranformConstructorArgs, model: GLTFShape) {
+    super()
+    this.addComponent(model)
+    this.addComponent(new Transform(transform))
+    engine.addEntity(this)
+    this.setParent(pedestal)
 
-	  // glow
-	  const rayCubeObject = new Entity()
-	  rayCubeObject.setParent(this)
-	  let rayShape = new BoxShape()
-	  rayCubeObject.addComponent(rayShape )
-	  this.glow = rayShape
-	  this.glow.withCollisions = false
-	  this.glow.visible = false
-	  rayCubeObject.addComponent(new Transform({
-		position: new Vector3(0, 10, 0),
-		scale: new Vector3(0.5, 20, 0.5)
-	  }))
-	  rayCubeObject.addComponentOrReplace(rayMaterial)
-	  engine.addEntity(rayCubeObject) 
-	}
-	public invokeCreature(): void {
-		if(creature.currentState == CreatureState.Hunting) return
+    // glow
+    const rayCubeObject = new Entity()
+    rayCubeObject.setParent(this)
+    let rayShape = new BoxShape()
+    rayCubeObject.addComponent(rayShape)
+    this.glow = rayShape
+    this.glow.withCollisions = false
+    this.glow.visible = false
+    rayCubeObject.addComponent(
+      new Transform({
+        position: new Vector3(0, 10, 0),
+        scale: new Vector3(0.5, 20, 0.5),
+      })
+    )
+    rayCubeObject.addComponentOrReplace(rayMaterial)
+    engine.addEntity(rayCubeObject)
+  }
+  public invokeCreature(): void {
+    if (creature.currentState == CreatureState.Hunting) return
 
-		creature.getInvoked()
-		scatterPages(totalPages)
-		this.removeGlow()		
-	}
-	public trapCreature(): void {
-		if(creature.currentState == CreatureState.Trapped) return
+    creature.getInvoked()
+    scatterPages(totalPages)
+    this.removeGlow()
+  }
+  public trapCreature(): void {
+    if (creature.currentState == CreatureState.Trapped) return
 
-		creature.getTrapped()
-		prepareDancingMika()
-		addCandles()
-		this.removeGlow()
-	}
+    creature.getTrapped()
+    prepareDancingMika()
+    addCandles()
+    this.removeGlow()
+  }
 
-	public activateGlow(): void {
-		this.glow.visible = true
-	}
+  public activateGlow(): void {
+    this.glow.visible = true
+  }
 
-	public removeGlow(): void {
-		this.glow.visible = false
-	}
-
+  public removeGlow(): void {
+    this.glow.visible = false
+  }
 }
 
-
 export const book = new Book(
-	{ position: new Vector3(0, 2.75, -0.55),
-	  rotation: new Quaternion(0, 0, 0, 1),
-	  scale: new Vector3(2, 2, 2)
-	},
-	new GLTFShape('models/Book_05/Book_05.glb')
+  {
+    position: new Vector3(0, 2.75, -0.55),
+    rotation: new Quaternion(0, 0, 0, 1),
+    scale: new Vector3(2, 2, 2),
+  },
+  new GLTFShape('models/Book_05/Book_05.glb')
 )
 
-book.addComponentOrReplace(new OnPointerDown(e=>{
-	if (e.hit.length > 4) {return}
-	if (hasAllPages && creature.currentState == CreatureState.Hunting){
-		// book.trapCreature()
-		releaseMicasHead()
-	}
-}))
+book.addComponentOrReplace(
+  new OnPointerDown((e) => {
+    if (e.hit.length > 4) {
+      return
+    }
+    if (hasAllPages && creature.currentState == CreatureState.Hunting) {
+      // book.trapCreature()
+      releaseMicasHead()
+    }
+  })
+)
--- src/builderScene.ts
+++ src/builderScene.ts
@@ -1,71 +1,78 @@
-
-
 export const domeEntity = new Entity()
 export const domeShape = new GLTFShape('models/Dome.glb')
 domeShape.withCollisions = false
 domeEntity.addComponentOrReplace(domeShape)
-domeEntity.addComponentOrReplace(new Transform({
-	position: new Vector3(32, 0, 32),
-	scale: new Vector3(1, 1, 1)
-}))
+domeEntity.addComponentOrReplace(
+  new Transform({
+    position: new Vector3(32, 0, 32),
+    scale: new Vector3(1, 1, 1),
+  })
+)
 engine.addEntity(domeEntity)
 
 // Forest
 const forest = new Entity()
 forest.addComponentOrReplace(new GLTFShape('models/Forest.glb'))
-forest.addComponentOrReplace(new Transform({
-	position: new Vector3(32, 0, 32),
-	rotation: Quaternion.Euler(0, 180, 0),
-	scale: new Vector3(1, 1, 1)
-}))
+forest.getComponent(GLTFShape).isPointerBlocker = false
+forest.addComponentOrReplace(
+  new Transform({
+    position: new Vector3(32, 0, 32),
+    rotation: Quaternion.Euler(0, 180, 0),
+    scale: new Vector3(1, 1, 1),
+  })
+)
 engine.addEntity(forest)
 
 // Fog
 const fog = new Entity()
 fog.addComponentOrReplace(new GLTFShape('models/Fog.glb'))
-fog.addComponentOrReplace(new Transform({
-	position: new Vector3(32, 0, 32),
-	scale: new Vector3(1, 1, 1)
-}))
+fog.addComponentOrReplace(
+  new Transform({
+    position: new Vector3(32, 0, 32),
+    scale: new Vector3(1, 1, 1),
+  })
+)
 
-const fogAnim = new Animator();
-fog.addComponent(fogAnim);
-let fogAnimation = new AnimationState("fog", { speed: 0.15 })
-fogAnim.addClip(fogAnimation);
+const fogAnim = new Animator()
+fog.addComponent(fogAnim)
+let fogAnimation = new AnimationState('fog', { speed: 0.15 })
+fogAnim.addClip(fogAnimation)
 fogAnimation.play()
 
 engine.addEntity(fog)
 
-
 // Flys
 const flys1 = new Entity()
 flys1.addComponentOrReplace(new GLTFShape('models/flys.glb'))
-flys1.addComponentOrReplace(new Transform({
-	position: new Vector3(38, 0.5, 37),
-	scale: new Vector3(1, 1, 1)
-}))
+flys1.addComponentOrReplace(
+  new Transform({
+    position: new Vector3(38, 0.5, 37),
+    scale: new Vector3(1, 1, 1),
+  })
+)
 
-const flys1Anim = new Animator();
-flys1.addComponent(flys1Anim);
-let flys1Animation = new AnimationState("fly", { speed: 1 })
-flys1Anim.addClip(flys1Animation);
+const flys1Anim = new Animator()
+flys1.addComponent(flys1Anim)
+let flys1Animation = new AnimationState('fly', { speed: 1 })
+flys1Anim.addClip(flys1Animation)
 flys1Animation.play()
 
 engine.addEntity(flys1)
 
-
 // Flys
 const flys2 = new Entity()
 flys2.addComponentOrReplace(new GLTFShape('models/flys.glb'))
-flys2.addComponentOrReplace(new Transform({
-	position: new Vector3(55, 1, 14),
-	scale: new Vector3(1, 1, 1)
-}))
+flys2.addComponentOrReplace(
+  new Transform({
+    position: new Vector3(55, 1, 14),
+    scale: new Vector3(1, 1, 1),
+  })
+)
 
-const flys2Anim = new Animator();
-flys2.addComponent(flys2Anim);
-let flys2Animation = new AnimationState("fly", { speed: 0.9 })
-flys2Anim.addClip(flys2Animation);
+const flys2Anim = new Animator()
+flys2.addComponent(flys2Anim)
+let flys2Animation = new AnimationState('fly', { speed: 0.9 })
+flys2Anim.addClip(flys2Animation)
 flys2Animation.play()
 
 engine.addEntity(flys2)
@@ -73,26 +80,27 @@ engine.addEntity(flys2)
 // Flys3
 const flys3 = new Entity()
 flys3.addComponentOrReplace(new GLTFShape('models/flys.glb'))
-flys3.addComponentOrReplace(new Transform({
-	position: new Vector3(28, 2, 48),
-	scale: new Vector3(1, 1, 1)
-}))
+flys3.addComponentOrReplace(
+  new Transform({
+    position: new Vector3(28, 2, 48),
+    scale: new Vector3(1, 1, 1),
+  })
+)
 
-const flys3Anim = new Animator();
-flys3.addComponent(flys3Anim);
-let flys3Animation = new AnimationState("fly", { speed: 0.8 })
-flys3Anim.addClip(flys3Animation);
+const flys3Anim = new Animator()
+flys3.addComponent(flys3Anim)
+let flys3Animation = new AnimationState('fly', { speed: 0.8 })
+flys3Anim.addClip(flys3Animation)
 flys3Animation.play()
 
 engine.addEntity(flys3)
 
-
 /// builder stuff
 const scene = new Entity()
 const transform = new Transform({
-	position: new Vector3(0, 0, 0),
-	rotation: new Quaternion(0, 0, 0, 1),
-	scale: new Vector3(1, 1, 1)
+  position: new Vector3(0, 0, 0),
+  rotation: new Quaternion(0, 0, 0, 1),
+  scale: new Vector3(1, 1, 1),
 })
 scene.addComponentOrReplace(transform)
 engine.addEntity(scene)
@@ -102,10 +110,9 @@ forestFloor.setParent(scene)
 const gltfShape_3 = new GLTFShape('models/Floor.glb')
 forestFloor.addComponentOrReplace(gltfShape_3)
 const transform_4 = new Transform({
-	position: new Vector3(32, 0, 32),
-	rotation: Quaternion.Euler(0, 180, 0),
-	scale: new Vector3(1, 1, 1)
+  position: new Vector3(32, 0, 32),
+  rotation: Quaternion.Euler(0, 180, 0),
+  scale: new Vector3(1, 1, 1),
 })
 forestFloor.addComponentOrReplace(transform_4)
 engine.addEntity(forestFloor)
-
